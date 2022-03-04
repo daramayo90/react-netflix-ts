adbcb5367998d548d6a49af81899ac59852fcb1f
daramayo90 <da.aramayo1990@gmail.com>
2022-03-03 20:21:52 -0500

    react-netflix-ts: movie thumbnail image description added

--- src/pages/browse/Browse.tsx
+++ src/pages/browse/Browse.tsx
@@ -5,9 +5,10 @@ function Browse() {
   return (
     <div className="browse-container">
       <div className="thumbnail-section">
-        <div className="thumbnail" style={{ backgroundImage: `url(assets/images/popcorn.jpg)` }}>
+        <div className="thumbnail" style={{ backgroundImage: `linear-gradient(to right, rgba(0, 0, 0, 0.7) 15%, transparent 60%), url(assets/images/popcorn.jpg)` }}></div>
+        <span className="description">
 
-        </div>
+        </span>
       </div>
     </div>
   );
