b2cf5b49e2414e9f29d6ea7e6498a7bb0c3ef6e7
daramayo90 <da.aramayo1990@gmail.com>
2022-03-03 20:05:46 -0500

    react-netflix-ts: thumbail image in browser added

--- src/pages/browse/Browse.tsx
+++ src/pages/browse/Browse.tsx
@@ -1,10 +1,16 @@
 import React from "react";
-import './Browse.css'
+import "./Browse.css";
 
-function Browse () {
-    return (
-        <div></div>
-    )
+function Browse() {
+  return (
+    <div className="browse-container">
+      <div className="thumbnail-section">
+        <div className="thumbnail" style={{ backgroundImage: `url(assets/images/popcorn.jpg)` }}>
+
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Browse;
