0ed08096e5e1b3e5ac0ded337d50b5c8e0144a8f
daramayo90 <da.aramayo1990@gmail.com>
2022-03-06 14:51:01 -0500

    react-netflix-ts: Navbar profile settings - CSS

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./Navbar.css";
 
@@ -13,6 +13,13 @@ interface Props {
 }
 
 function Navbar({ only_logo, profile_url }: Props) {
+  const [isScrolled, setIsScrolled] = useState<boolean>(false);
+
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
+
   if (only_logo) {
     return (
       <div className="navbar-container">
@@ -21,21 +28,31 @@ function Navbar({ only_logo, profile_url }: Props) {
     );
   } else {
     return (
-      <div className="navbar-container">
-        <img className="logo" src="assets/images/netflix.svg" />
-        <button className="button-tertiary button-tertiary-active">Home</button>
-        <button className="button-tertiary">Series</button>
-        <button className="button-tertiary">Films</button>
-        <button className="button-tertiary">Latest</button>
-        <button className="button-tertiary">My List</button>
+      <div className={isScrolled ? "scrolled" : "navbar"}>
+        <div className="navbar-container">
+          <img className="logo" src="assets/images/netflix.svg" />
+          <button className="button-tertiary button-tertiary-active">
+            Home
+          </button>
+          <button className="button-tertiary">Series</button>
+          <button className="button-tertiary">Films</button>
+          <button className="button-tertiary">Latest</button>
+          <button className="button-tertiary">My List</button>
 
-        <div className="spacer"></div>
+          <div className="spacer"></div>
 
-        <SearchIcon />
-        <CardGiftIcon />
-        <NotificationsIcon />
-        {profile_url && <img className="profile-image" src={profile_url} />}
-        <ArrowDropDownIcon />
+          <SearchIcon />
+          <CardGiftIcon />
+          <NotificationsIcon />
+          {profile_url && <img className="profile-image" src={profile_url} />}
+          <div className="profile">
+            <ArrowDropDownIcon className="icon" />
+            <div className="options">
+              <span>Settings</span>
+              <span>Logout</span>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
